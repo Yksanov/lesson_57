e0ec9270db3ed95f85e358ac50cb549065bd9f5e
Eldos Yksanov <eldosyksanov@gmail.com>
2024-11-07 18:56:36 +0600

    change Layout and check solutions

--- ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e83c5154d49104e44000599b55a934142c5eb87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f33193f21d08570ace7400c43d469e1d8298ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
