d0bc6406b11daca675b0404a1012025465929752
Eldos Yksanov <eldosyksanov@gmail.com>
2024-12-03 16:58:43 +0600

    changed password to postgres database

--- ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0ec9270db3ed95f85e358ac50cb549065bd9f5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df96ae962d8a6f32d8abcaffe67409f109a3b77a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
