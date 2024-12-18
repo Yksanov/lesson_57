6c73187e7399f089920f78e77155ee2d0639e68e
Eldos Yksanov <eldosyksanov@gmail.com>
2024-12-18 13:20:18 +0600

    changed migrations and some changed DbContext

--- ToDoList/Migrations/20241218035504_changedUserI.Designer.cs
+++ ToDoList/Migrations/20241218035504_changedUserI.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ToDoList.Models;
@@ -11,9 +12,11 @@ using ToDoList.Models;
 namespace ToDoList.Migrations
 {
     [DbContext(typeof(TaskStoreContext))]
-    partial class TaskStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20241218035504_changedUserI")]
+    partial class changedUserI
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
